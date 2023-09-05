a25a46ecb0b323116644120e42298a92e363befc
joshitrejimon1 <joshit.rejimon@aurionpro.com>
2023-09-05 13:00:27 +0530

    Add project files.

---
+++ LineItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineItemList
+{
+    internal class LineItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public double UnitPrice {get; set; }
+        public double ItemCost { get { return UnitPrice * Quantity; } }
+
+     public LineItem(int id, string name, double quantity, double unitPrice)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}  {UnitPrice} x {Quantity}={ItemCost}";
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LineItemList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<LineItem> items = new List<LineItem>
+          { new LineItem(101,"MilkBikies",2,50),
+            new LineItem(102, "Diary Milk", 2, 100),
+            new LineItem(103, "Book", 3, 100)
+
+
+          };
+            PrintItems(items);
+
+
+
+
+
+
+        }
+
+        private static void PrintItems(List<LineItem> items)
+        {
+            double totalCartCost = 0;
+            Console.WriteLine("===============order dertails==============");
+            foreach (LineItem item in items)
+            {
+                Console.WriteLine(item.ToString());
+                totalCartCost += item.ItemCost;
+
+            }
+            Console.WriteLine("==============");
+            Console.WriteLine("total cart cost" + totalCartCost);
+        }
+    }
+}
